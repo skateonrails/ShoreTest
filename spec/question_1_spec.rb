49a1069335cd9c67b143cb89d56bef0ff5ca956b
Samuel Prado <darkms@gmail.com>
2016-10-08 11:17:11 -0300

    Improve question_1_spec.rb file using let method

--- spec/question_1_spec.rb
+++ spec/question_1_spec.rb
@@ -2,15 +2,17 @@ require File.join(File.dirname(__FILE__), '..', 'question_1.rb')
 
 describe CharacterFrequency do
   context "given an array of charactes" do
+    let(:input){ ['a', 'a', 'b', 'c', 'b', 'd', 'c', 'c'] }
+
     it "shows frequency of each character" do
-      input = ['a', 'a', 'b', 'c', 'b', 'd', 'c', 'c']
       expect(CharacterFrequency.new(input).to_hash).to eq({'a' => 2, 'b' => 2, 'c' => 3, 'd' => 1})
     end
   end
 
   context "given an input that is not an array" do
+    let(:input) { "string" }
+
     it "raise InvalidParameter error" do
-      input = "string"
       expect{CharacterFrequency.new(input)}.to raise_error(CharacterFrequency::InvalidParameter)
     end
   end
